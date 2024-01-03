9bd9f692ce3670f380037d0b6adf9f6b942fcd5c
shisi.eth <yi.lu1@okg.com>
2024-01-03 10:19:15 +0800

    Test atomic protocol assembly

--- packages/coin-bitcoin/src/wallet/AtomicalWallet.ts
+++ packages/coin-bitcoin/src/wallet/AtomicalWallet.ts
@@ -10,19 +10,15 @@ export class AtomicalWallet extends BtcWallet {
 
     convert2AtomicalTx(paramData: any): utxoTx {
         const clonedParamData = cloneObject(paramData)
-
-        // cal atomical token input all amount
-        let inputs = clonedParamData.inputs;
         const atomicalInputMap = new Map<string, number>();
-        // const atomicalInputMapList: Map<string, number>[] = [];
         const atomicalTypeMap = new Map<string, string>();
         let txInput = []
         let txOutput = []
-        // let txInputIndex = 0
 
         // Calculate the total Atomical asset input value from the 'input' field
         // and construct the 'input' field for the UTXO (Unspent Transaction Output).
         // The atomicalInputMapList is used to check whether a complete transfer is achieved in the 'output'.
+        let inputs = clonedParamData.inputs;
         for (const input of inputs) {
             let dataArray = input.data;
             if (dataArray != null && dataArray instanceof Array) {
@@ -35,9 +31,9 @@ export class AtomicalWallet extends BtcWallet {
                     if (atomicalId == null || atomicalAmount == null  || atomicalIdType == null) {
                         continue
                     }
-                    // if (atomicalIdType != "FT" && atomicalIdType != "NFT" ){
-                    //     continue
-                    // }
+                    if (atomicalIdType != "FT" && atomicalIdType != "NFT" ){
+                        continue
+                    }
 
                     if (atomicalTypeMap.get(atomicalId) == null) {
                         atomicalTypeMap.set(atomicalId, atomicalIdType);
@@ -46,21 +42,16 @@ export class AtomicalWallet extends BtcWallet {
                     let beforeAmount = atomicalInputMap.get(atomicalId);
                     if (beforeAmount == null) {
                         atomicalInputMap.set(atomicalId, atomicalAmount);
-                        // atomicalInputMapList[txInputIndex].set(atomicalId, atomicalAmount);
                     } else {
                         atomicalInputMap.set(atomicalId, beforeAmount + atomicalAmount);
-                        // atomicalInputMapList[txInputIndex].set(atomicalId, beforeAmount + atomicalAmount);
                     }
                 }
-                // txInputIndex ++;
-
             }
             txInput.push({
                 txId:input.txId,
                 vOut:input.vOut,
                 amount:input.amount,
                 address:input.address,
-                // privateKey:input.privateKey,
             })
         }
 
@@ -73,7 +64,6 @@ export class AtomicalWallet extends BtcWallet {
         const atomicalSendMap = new Map<string, number>();
         for (const output of outputs) {
             let dataArray = output.data;
-            // let atomUtxoIndex = 0
             if (dataArray != null && dataArray instanceof Array) {
 
                 for (const data of dataArray) {
@@ -98,14 +88,7 @@ export class AtomicalWallet extends BtcWallet {
                     } else {
                         atomicalSendMap.set(atomicalId, beforeAmount + atomicalAmount);
                     }
-
-                    // let beforeUtxoAmount = atomicalInputMapList[atomUtxoIndex].get(atomicalId)
-                    // if (beforeUtxoAmount != null && beforeUtxoAmount >= output.amount){
-                    //     atomicalInputMapList[atomUtxoIndex].set(atomicalId,beforeUtxoAmount - atomicalAmount);
-                    // }
-
                 }
-                // atomUtxoIndex ++;
             }
             txOutput.push({
                 amount:output.amount,
@@ -131,12 +114,10 @@ export class AtomicalWallet extends BtcWallet {
                     throw new Error(`Error: Output does not meet dust limit conditions. amount: ${changeAmount}.`);
                 }
 
-                let atomicalChange = {
+                txOutput.push({
                     address:clonedParamData.changeAddress,
                     amount:changeAmount
-                }
-                txOutput.push(atomicalChange)
-
+                })
             } else if (inputAmount != null && sendAmount != null && inputAmount < sendAmount){ 
                 throw new Error(`Error: AtomicalId ${atomicalId} has insufficient input amount \
                     (${inputAmount}) compared to the output amount (${sendAmount}).\
@@ -172,8 +153,6 @@ export class AtomicalWallet extends BtcWallet {
         try {
             const privateKey = param.privateKey;
             const atomicalTx = this.convert2AtomicalTx(param.data);
-
-            console.log(atomicalTx)
 
             txHex = signBtc(atomicalTx, privateKey, network);
             return Promise.resolve(txHex);
